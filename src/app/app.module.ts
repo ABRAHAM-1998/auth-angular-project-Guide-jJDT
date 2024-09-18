aa4ba356d431b1472a609e1f9804015d7a9d209f
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2024-09-18 17:54:14 +0530

    ADDED ANGUA;R MARTERIAL AND FIREBASE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({"projectId":"sevenzeronine-test","appId":"1:808404753539:web:9cac146464a0bd0ba04fb6","storageBucket":"sevenzeronine-test.appspot.com","apiKey":"AIzaSyCjaMQ5hbLm9J6FtecKM6j7T156OSR5PMk","authDomain":"sevenzeronine-test.firebaseapp.com","messagingSenderId":"808404753539","measurementId":"G-84GSC7J0PS"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
